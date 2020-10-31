68b01076d84774feabae03e40cc38ffa62daf510
Ryan Chase <ryan@chasery.com>
2020-10-31 13:47:14 -0700

    Initialize basic app structure

---
+++ index.js
@@ -0,0 +1,11 @@
+function getMedia() {
+  // Get NASA's media coverage of historical events
+  console.log("`getMedia` ran");
+}
+
+function getHistoricalEvents() {
+  // Get SpaceX's list of historical events
+  console.log("`getHistoricalEvents` ran");
+}
+
+$(getHistoricalEvents);
