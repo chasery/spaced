c194fe0a0de2c1f11c718e1b1dff985ef01a082f
Ryan Chase <ryan@chasery.com>
2020-11-01 12:01:32 -0800

    Render sorted list of events by date

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 // SpaceX Historical Events Endpoint URL
-const spacexHistoryEndpoint = "https://api.spacexdata.com/v4/histor";
+const spacexHistoryEndpoint = "https://api.spacexdata.com/v4/history";
 
 function getMedia() {
   // Get NASA's media coverage of historical events
   console.log("`getMedia` ran");
+}
+
+function renderHistoricalEvents(sortedEvents) {
+  // Renders each of the  events in the sorted array
+  const eventsContainer = $("#jsEventsList");
+  eventsContainer.removeClass("hidden");
+  for (let i = 0; i < sortedEvents.length; i++) {
+    const event = sortedEvents[i];
+    eventsContainer.children("ul").append(`
+      <li class="event" data-id="${event.id}">
+        <h2 class="event__title">${event.title}</h2>
+        <h3 class="event__date">${event.event_date_utc}</h3>
+      </li>
+    `);
+  }
+}
+
+function sortHistoricalEvents(events) {
+  const sortedEvents = events
+    .slice()
+    .sort((a, b) => new Date(b.event_date_utc) - new Date(a.event_date_utc));
+  return sortedEvents;
 }
 
 function getHistoricalEvents() {
@@ -21,7 +43,7 @@ function appInit() {
   // Initialize appropriate functions
   getHistoricalEvents()
     .then((json) => {
-      console.log(json);
+      renderHistoricalEvents(sortHistoricalEvents(json));
     })
     .catch((status) => {
       console.log(status);
