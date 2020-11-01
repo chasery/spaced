533ba7f09dab5aad1dce0314b83a8b2645554530
Ryan Chase <ryan@chasery.com>
2020-11-01 11:06:22 -0800

    Return SpaceXs history endpoint results

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// SpaceX Historical Events Endpoint URL
+const spacexHistoryEndpoint = "https://api.spacexdata.com/v4/history";
+
 function getMedia() {
   // Get NASA's media coverage of historical events
   console.log("`getMedia` ran");
@@ -6,11 +9,19 @@ function getMedia() {
 function getHistoricalEvents() {
   // Get SpaceX's list of historical events
   console.log("`getHistoricalEvents` ran");
+  return fetch(spacexHistoryEndpoint).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    return response.status;
+  });
 }
 
 function appInit() {
   // Initialize appropriate functions
-  getHistoricalEvents();
+  getHistoricalEvents().then((json) => {
+    console.log(json);
+  });
 }
 
 $(appInit);
