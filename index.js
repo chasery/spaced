4f59042313b4082ecbdcec08d301c2f82b98fe4d
Ryan Chase <ryan@chasery.com>
2020-11-01 21:04:51 -0800

    Add in loading indicators

--- index.js
+++ index.js
@@ -4,17 +4,42 @@ const spacexHistoryEndpoint = "https://api.spacexdata.com/v4/history";
 function getMedia() {
   // Get NASA's media coverage of historical events
   console.log("`getMedia` ran");
-}
-
-function handleEventClick() {
-  $("#jsEventsList").on("click", "#jsEventItem", function (event) {
-    const target = $(event.currentTarget);
-    target.siblings().removeClass("active");
-    target.toggleClass("active");
+  return fetch(spacexHistoryEndpoint).then((response) => {
+    if (response.ok) {
+      return "hi";
+    }
+    return response.status;
   });
 }
 
-function formatDateString(date) {
+function createLoader() {
+  return `
+    <div class="loader">
+      <div class="loader-circle">
+        <div>
+          <div class="loader moon-loader"></div>
+        </div>
+      </div>
+      <div class="loader-sun"></div>
+    </div>
+  `;
+}
+
+function handleEventClick() {
+  // Handles the click event on an event
+  $("#jsEventsList").on("click", "#jsEventItem", function (event) {
+    const target = $(event.currentTarget);
+    target.toggleClass("active").siblings().removeClass("active");
+    target.find("#jsNasaContent").html(createLoader());
+    getMedia().then((json) => {
+      setTimeout(function () {
+        target.find("#jsNasaContent").html(json);
+      }, 2000);
+    });
+  });
+}
+
+function formatDisplayDate(date) {
   // Returns a human legible date string
   const dateObj = new Date(date);
   const year = dateObj.getUTCFullYear();
@@ -26,12 +51,11 @@ function formatDateString(date) {
 
 function renderHistoricalEvents(sortedEvents) {
   // Renders each of the  events in the sorted array
-  const eventsContainer = $("#jsEventsList");
-  eventsContainer.removeClass("hidden");
+  const eventsContainer = $("#jsEventsList ul");
   for (let i = 0; i < sortedEvents.length; i++) {
     const event = sortedEvents[i];
-    const eventDate = formatDateString(event.event_date_utc);
-    eventsContainer.children("ul").append(`
+    const eventDate = formatDisplayDate(event.event_date_utc);
+    eventsContainer.append(`
       <li id="jsEventItem" class="event" data-id="${event.id}">
         <div class="event__header">
           <div class="event__headerContent">
@@ -42,6 +66,7 @@ function renderHistoricalEvents(sortedEvents) {
         </div>
         <div class="event__body">
           <p>${event.details}</p>
+          <div id="jsNasaContent" class="event__nasaContent"></div>
         </div>
       </li>
     `);
@@ -49,6 +74,7 @@ function renderHistoricalEvents(sortedEvents) {
 }
 
 function sortHistoricalEvents(events) {
+  // Sorts SpaceX's historical events from newest to oldest
   const sortedEvents = events
     .slice()
     .sort((a, b) => new Date(b.event_date_utc) - new Date(a.event_date_utc));
@@ -66,11 +92,22 @@ function getHistoricalEvents() {
   });
 }
 
+function toggleHidden(element) {
+  // Toggles the hidden class for the provided element
+  element.toggleClass("hidden");
+}
+
 function appInit() {
   // Initialize appropriate functions
   getHistoricalEvents()
     .then((json) => {
-      renderHistoricalEvents(sortHistoricalEvents(json));
+      const eventsContainer = $("#jsEventsList");
+      const sortedEvents = sortHistoricalEvents(json);
+      setTimeout(function () {
+        toggleHidden(eventsContainer.children("div.eventList__loader"));
+        renderHistoricalEvents(sortedEvents);
+        toggleHidden(eventsContainer.children("ul.eventsList__wrapper"));
+      }, 2000);
     })
     .catch((status) => {
       console.log(status);
